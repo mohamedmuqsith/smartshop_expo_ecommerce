edd95bbbc7650e6f133d8c5282a3c8ca1a4d9287
Mukshith <mohamedmukshidh35@gmail.com>
2026-02-09 19:16:04 +0530

    db config & tools setup

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -5,5 +5,11 @@ dotenv.config(); //Read the .env file and load the enviroment variables into pro
 export const ENV ={
     NODE_ENV: process.env.NODE_ENV || "development",
     PORT: process.env.PORT || 5000,
-    DB_URL: process.env.DB_URL
+    DB_URL: process.env.DB_URL,
+    CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
 }   
