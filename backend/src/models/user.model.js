74110e7a6e7b76931915d448a467088ff78ee953
Mukshith <mohamedmukshidh35@gmail.com>
2026-02-09 21:23:25 +0530

    auth implemented

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+
+const addressSchema = new mongoose.Schema({
+    label:{
+        type:String,
+        required:true
+    },
+    fullName:{
+        type:String,
+        required:true
+    },
+    StreetAddress:{
+        type:String,
+        required:true
+    },
+    city:{
+        type:String,
+        required:true
+    },
+    province:{
+        type:String,
+        required:true
+    },
+    zipCode:{
+        type:String,
+        required:true
+    },
+    phoneNumber:{
+        type:String,
+        required:true
+    },
+    isDefault:{
+        type:Boolean,
+        default:false
+    }
+})
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+     name:{
+        type:String,
+        required:true
+     },
+     imageURL:{
+        type:String,
+        default:""
+     },
+     clerkId:{
+        type:String,
+        unique:true,
+        required:true
+     },
+     addresses:[addressSchema],
+     wishlist:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Product"
+        }
+     ],
+}, {timestamps:true}
+)
+
+export const User = mongoose.model("User", userSchema)
